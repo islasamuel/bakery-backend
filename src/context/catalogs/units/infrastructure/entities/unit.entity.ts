c385a2d3b2bd615c6619fd38808574c3c9418eb9
Samuel Islas Mateos <samuelislas@outlook.com>
2024-11-24 20:35:15 -0500

    Update unit.entity.ts
    Update int to uuid

--- src/context/catalogs/units/infrastructure/entities/unit.entity.ts
+++ src/context/catalogs/units/infrastructure/entities/unit.entity.ts
@@ -1,5 +1,5 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 /**
  * Represents a unit in the catalog of units.
@@ -7,9 +7,9 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @ObjectType({ description: 'Represents a unit in the catalog of units.' })
 @Entity({ name: 'cat_units' })
 export class Unit {
-  @Field(() => Int, { description: 'Unique identifier of the unit.' })
-  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
-  id: number;
+  @Field(() => String, { description: 'Unique identifier of the unit.' })
+  @PrimaryColumn({ type: 'uuid', name: 'id' })
+  id: string;
 
   @Field(() => String, { description: 'Description of the unit.' })
   @Column({ type: 'varchar', name: 'description', length: 100 })
